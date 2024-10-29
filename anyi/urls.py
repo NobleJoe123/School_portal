12c4d0cb459893bf4bdeac89fac25419fc78bd28
NobleJoe123 <Joshuachibuike14@gmail.com>
2024-10-29 14:32:50 +0100

    add

--- anyi/urls.py
+++ anyi/urls.py
@@ -1,6 +1,6 @@
 
 from django.urls import path
-from .views import home, about, courses, admin_register, admin_login, student_login, bursal_login, teacher_login, enrol, user_enrol, student_dashboard,teacher_dashboard, admin_dashboard, bursal_dashboard
+from . import views
 # search_create
 
 
@@ -9,20 +9,20 @@ from .views import home, about, courses, admin_register, admin_login, student_lo
 
 
 urlpatterns = [
-    path('', home, name='home'),
-    path('about/', about, name='about'),
-    path('courses/', courses, name='courses'),
-    path('admin_register/', admin_register, name='admin_register'),
-    path('admin_login/', admin_login, name='admin_login'),
-    path('student_login/', student_login, name='student_login'),
-    path('bursal_login/', bursal_login, name='bursal_login'),
-    path('teacher_login/', teacher_login, name='teacher_login'),
-    path('admin_dashboard/', admin_dashboard, name='admin_dashboard'),
-    path('student_dashboard/', student_dashboard, name='student_dashboard'),
-    path('bursal_dashboard/', bursal_dashboard, name='bursal_dashboard'),
-    path('teacher_dashboard/', teacher_dashboard, name='teacher_dashboard'),
-    path('enrol/', enrol, name='enrol'),
-    path('user_enrol/', user_enrol, name='user_enrol'),
+    path('', views.home, name='home'),
+    path('about/', views.about, name='about'),
+    path('courses/', views.courses, name='courses'),
+    path('admin_register/', views.admin_register, name='admin_register'),
+    path('admin_login/', views.admin_login, name='admin_login'),
+    path('student_login/', views.student_login, name='student_login'),
+    path('bursal_login/', views.bursal_login, name='bursal_login'),
+    path('teacher_login/', views.teacher_login, name='teacher_login'),
+    path('admin_dashboard/', views.admin_dashboard, name='admin_dashboard'),
+    path('student_dashboard/', views.student_dashboard, name='student_dashboard'),
+    path('bursal_dashboard/', views.bursal_dashboard, name='bursal_dashboard'),
+    path('teacher_dashboard/', views.teacher_dashboard, name='teacher_dashboard'),
+    path('enrol/', views.enrol, name='enrol'),
+    path('user_enrol/', views.user_enrol, name='user_enrol'),
 
    
 ]
