f2b42714bdfebe1c8a9ab53e5ade00f2b22af9f7
NobleJoe123 <Joshuachibuike14@gmail.com>
2024-10-31 09:37:02 +0100

    add

--- anyi/forms.py
+++ anyi/forms.py
@@ -1,6 +1,6 @@
 from django import forms
 from django.core.exceptions import ValidationError
-from .models import Role, Admin, Bursal, Teacher
+from .models import Role, jss1, jss2, jss3, ss1, ss2, ss3
 
 class UserForm(forms.Form):
     fname = forms.CharField(max_length=100)
@@ -15,3 +15,51 @@ class UserForm(forms.Form):
 class LoginForm(forms.Form):
     username = forms.CharField(max_length=100)
     password = forms.CharField(widget=forms.PasswordInput)
+
+
+
+
+class StudentForm(forms.Form):
+    STUDENT_TYPE_CHOICES = [
+        ('jss1', 'jss1'),
+        ('jss2', 'jss2'),
+        ('jss3', 'jss3'),
+        ('ss1', 'ss1'),
+        ('ss2', 'ss2'),
+        ('ss3', 'ss3'),
+    ]
+
+    firstname = forms.CharField(max_length=100)
+    surname = forms.CharField(max_length=100)
+    middlename = forms.CharField(max_length=100, required=False)
+    username = forms.CharField(max_length=100)
+    email = forms.EmailField()
+    phonenum = forms.CharField(max_length=15)
+    password = forms.CharField(widget=forms.PasswordInput)
+    state = forms.CharField(max_length=100)
+    country = forms.CharField(max_length=100)
+    lga = forms.CharField(max_length=100)
+    dob = forms.DateField(widget=forms.DateInput(attrs={'type': 'date'}))
+    guardianname = forms.CharField(max_length=100)
+    classes = forms.ChoiceField(choices=STUDENT_TYPE_CHOICES)
+
+    def save(self):
+        # Get cleaned data from the form
+        data = self.cleaned_data
+        student_type = data.pop('classes')  # Extract the dropdown value
+
+        # Determine which model to use based on dropdown selection
+        if student_type == 'jss1':
+            return jss1.objects.create(**data)
+        elif student_type == 'jss2':
+            return jss2.objects.create(**data)
+        elif student_type == 'jss3':
+            return jss3.objects.create(**data)
+        elif student_type == 'ss1':
+            return ss1.objects.create(**data)
+        elif student_type == 'ss2':
+            return ss2.objects.create(**data)
+        elif student_type == 'ss3':
+            return ss3.objects.create(**data)
+        else:
+            raise ValueError("Invalid student type selected")   
