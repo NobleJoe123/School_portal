e68ed6666f3f9ef83ff2454f0c696d34e9e28816
NobleJoe123 <Joshuachibuike14@gmail.com>
2024-11-04 13:06:20 +0100

    add

--- anyi/models.py
+++ anyi/models.py
@@ -106,24 +106,24 @@ class jss3(models.Model):
     def __str__(self):
         return f"jss3: {self.username}"
     
-class ss1(models.Model):
-    firstname = models.CharField(max_length=100)
-    surname = models.CharField(max_length=100)
-    middlename = models.CharField(max_length=100, blank=True, null=True)
-    username = models.CharField(max_length=100, unique=True)
-    email = models.EmailField(max_length=255, unique=True)
-    phonenum = models.CharField(max_length=15)
-    password = models.CharField(max_length=255)
-    state = models.CharField(max_length=100)
-    country = models.CharField(max_length=100)
-    lga = models.CharField(max_length=100)
-    dob = models.DateField()
-    guardianname = models.CharField(max_length=100)
-    image = models.ImageField(upload_to='student_images/', blank=True, null=True)
-    datetime = models.DateTimeField(auto_now_add=True)
+# class ss1(models.Model):
+#     firstname = models.CharField(max_length=100)
+#     surname = models.CharField(max_length=100)
+#     middlename = models.CharField(max_length=100, blank=True, null=True)
+#     username = models.CharField(max_length=100, unique=True)
+#     email = models.EmailField(max_length=255, unique=True)
+#     phonenum = models.CharField(max_length=15)
+#     password = models.CharField(max_length=255)
+#     state = models.CharField(max_length=100)
+#     country = models.CharField(max_length=100)
+#     lga = models.CharField(max_length=100)
+#     dob = models.DateField()
+#     guardianname = models.CharField(max_length=100)
+#     image = models.ImageField(upload_to='student_images/', blank=True, null=True)
+#     datetime = models.DateTimeField(auto_now_add=True)
 
-    def __str__(self):
-        return f"ss1: {self.username}"
+#     def __str__(self):
+#         return f"ss1: {self.username}"
     
 class ss1(models.Model):
     firstname = models.CharField(max_length=100)
